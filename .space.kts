10706dff55929f6c92fe98c24485e4b08ea1fe74
Amir Shirdel <amirex128@gmail.com>
2022-11-16 13:26:30 +0000

    aa

--- .space.kts
+++ .space.kts
@@ -1,38 +1,26 @@
-job("build backend and front selloora image") {
-    container("openjdk:11") {
-    kotlinScript { api ->
-        api.space().projects.automation.deployments.start(
-            project = api.projectIdentifier(),
-            targetIdentifier = TargetIdentifier.Key("backend"),
-            version = "1.0.0",
-            // automatically update deployment status based on a status of a job
-            syncWithAutomationJob = true
-        )
+job("build dockerfile"){
+	startOn {
+        gitPush {
+            tagFilter {
+                +"release/*"
+            }
+        }
     }
-}
-    host("Build artifacts and a Docker image") {
-        // generate artifacts required for the image
+    git("selloora-backend/backend.git") {
+        cloneDir = "backend"
+    }
+    git("selloora-frontend/frontend.git") {
+        cloneDir = "frontend"
+    }
+    
+    container(displayName = "Show dirs", image = "ubuntu:latest") {
         shellScript {
             content = """
-                ./generateArtifacts.sh
+                echo Directory structure
+                ls -R /mnt/space/work
+                echo Working dir is
+                pwd
             """
-        }
-
-        dockerBuildPush {
-            // Docker context, by default, project root
-            context = "docker"
-            // path to Dockerfile relative to project root
-            // if 'file' is not specified, Docker will look for it in 'context'/Dockerfile
-            file = "docker/config/Dockerfile"
-            labels["vendor"] = "mycompany"
-            args["HTTP_PROXY"] = "http://10.20.30.1:123"
-
-            val spaceRepo = "mycompany.registry.jetbrains.space/p/prjkey/mydocker/myimage"
-            // image tags for 'docker push'
-            tags {
-                +"$spaceRepo:1.0.${"$"}JB_SPACE_EXECUTION_NUMBER"
-                +"$spaceRepo:latest"
-            }
         }
     }
 }
